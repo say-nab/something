5fa4551b4e1c3128ef883f26307faa958308f540
Say-nab <saynab.abdi999@gmail.com>
2022-10-02 09:11:09 +0400

    adding menu options

--- notepad.py
+++ notepad.py
@@ -9,6 +9,15 @@ content_text.place(x=0,y=0, relwidth=1, relheight=1)
 main_menu = tk.Menu(window)
 window.configure(menu= main_menu)
 
+file_menu = tk.Menu(main_menu)
+main_menu.add_cascade(label= 'File', menu = file_menu)
+
+save_menu = tk.Menu(main_menu)
+main_menu.add_cascade(label= 'Save', menu= save_menu)
+
+open_file= tk.Menu(main_menu)
+main_menu.add_cascade(label= 'Open', menu= open_file)
+
 
 
 
