d0b8e205d3cea9157e29d86df10dd05be6119023
Say-nab <saynab.abdi999@gmail.com>
2022-10-02 08:54:53 +0400

    making the menu

--- notepad.py
+++ notepad.py
@@ -6,6 +6,14 @@ window.geometry('600x600')
 content_text = tk.Text(window)
 content_text.place(x=0,y=0, relwidth=1, relheight=1)
 
+main_menu = tk.Menu(window)
+window.configure(menu= main_menu)
+
+
+
+
+
+
 
 
 
